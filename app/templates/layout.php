26e44549fadb6ee069739490c31350d6d42ad7be
unknown <vicentepalacios98@gmail.com>
2020-02-03 13:34:36 +0100

    progress in navbar - footer started

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -6,31 +6,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reserva de aulas</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="container">
-        <div class="menu-wrap">
-            <nav class="menu">
-                <div class="profile"><img src="img/user1.png" alt="Matthew Greenberg" /><span>Matthew Greenberg</span></div>
-                <div class="link-list">
-                    <a href="#"><span>Latest stories</span></a>
-                    <a href="#"><span>Your friends</span></a>
-                    <a href="#"><span>Personal Settings</span></a>
-                    <a href="#"><span>Security &amp; Privacy</span></a>
-                </div>
-                <div class="icon-list">
-                    <a href="#"><i class="fa fa-fw fa-home"></i></a>
-                    <a href="#"><i class="fa fa-fw fa-question-circle"></i></a>
-                    <a href="#"><i class="fa fa-fw fa-power-off"></i></a>
-                </div>
-            </nav>
-        </div>
+<nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-white">
+  <a class="navbar-brand" href="#"><img src="img/logo.png" id="logo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-        <?php echo isset($contenido) ? $contenido : "" ?>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Reservations</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Classrooms</a>
+      </li>
+    </ul>
+    <!--
+        <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+     -->
+  </div>
+</nav>
+
+<footer class="container-fluid">
+    <div class="row">
+        <div class="col-4">
+
+        </div>
+        <div class="col-4">
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Privacy</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Terms & Conditions</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-4">
+
+        </div>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="js/script.js"></script>
 </body>
 
 </html>
---
+++ web/js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $("#navbarSupportedContent li a").on("click",function(){
+        $("#navbarSupportedContent li a").removeClass("active");
+        $(this).addClass("active");
+    });
+        
+
+
+});
